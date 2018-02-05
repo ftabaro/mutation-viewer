db4824887dcf0243e1e15e586515f60665143a94
Francesco Tabaro <francesco.tabaro@gmail.com>
2018-02-05 18:32:49 +0200

    POST working. Missing data structure, missing CLI

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 #[macro_use]
 extern crate rouille;
 
-use rouille::{Response, start_server};
+use rouille::{Request, Response, start_server};
+use rouille::input::json_input;
+
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
 
 extern crate flate2;
-
 use flate2::bufread::GzDecoder;
 
-use io::Result;
+use io::{Result, Error, ErrorKind};
 use std::fs::{read_dir, canonicalize, File};
 use std::path::{Path, PathBuf};
 use std::io;
@@ -56,31 +61,8 @@ fn get_datasets(dir: &str) -> Result<Vec<Dataset>> {
     Ok(result)
 }
 
-fn build_index_template(available_datasets: Vec<Dataset>) -> String {
-    let mut html = load_asset("index.html");
-    let outer_list_template = String::from("<li>{{dataset_name}}<ul>{{vcf_list}}</ul></li>");
-    let inner_list_template = String::from("<li><a href=\"/{{dataset_name}}/{{vcf_name}}\">{{vcf_name}}</a></li>");
-
-    let mut outer_list = String::from("");
-    for dataset in available_datasets {
-        let mut inner_list = String::from("");
-        for filename in dataset.vcf {
-            let inner_list_element = inner_list_template
-                .replace("{{dataset_name}}", &dataset.name)
-                .replace("{{vcf_name}}", &filename);
-            inner_list.push_str(&inner_list_element);
-        }
-        let outer_list_element = outer_list_template
-            .replace("{{dataset_name}}", &dataset.name)
-            .replace("{{vcf_list}}", &inner_list);
-        outer_list.push_str(&outer_list_element);
-    }
-    html = html.replace("{{dataset_list}}", &outer_list);
-    html
-}
-
 fn check_existence(p: &Path) -> () {
-    if !p.is_file() {
+    if !p.exists() {
         match File::create(p) {
             Ok(_) => eprintln!("check_existence -> {} created.", p.to_str().unwrap()),
             Err(e) => eprintln!("check_existence -> {:?} - {:?}", p, e)
@@ -102,13 +84,6 @@ fn check_auxiliary_files(root: &String) -> () {
     ()
 }
 
-fn load_file(p: &Path) -> Result<String> {
-    let mut content = String::new();
-    let mut file = File::open(p)?;
-    file.read_to_string(&mut content)?;
-    Ok(content)
-}
-
 fn check_absolute_path(d: &str) -> PathBuf {
     let pbuf_dir = PathBuf::from(d);
     if pbuf_dir.is_relative() {
@@ -117,13 +92,29 @@ fn check_absolute_path(d: &str) -> PathBuf {
                 cwd.push(&pbuf_dir);
                 match canonicalize(cwd) {
                     Ok(pbuf_dir) => return pbuf_dir,
-                    Err(e) => eprintln!("{:?}", e)
+                    Err(e) => eprintln!("check_absolute_path -> {:?} - {:?}", d, e)
                 };
             }
             Err(e) => eprintln!("check_path -> {:?}", e)
         };
     }
     pbuf_dir
+}
+
+fn load_file(p: &Path) -> Result<String> {
+    if !p.exists() { return Err(Error::new(ErrorKind::NotFound, "file not found.")); }
+    let mut content = String::new();
+    if p.extension().unwrap() == "gz" {
+        let mut file = File::open(p)?;
+        let mut bytes = Vec::new();
+        file.read_to_end(&mut bytes)?;
+        let mut gz = GzDecoder::new(&bytes[..]);
+        gz.read_to_string(&mut content)?;
+    } else {
+        let mut file = File::open(p)?;
+        file.read_to_string(&mut content)?;
+    }
+    Ok(content)
 }
 
 fn load_asset(filename: &str) -> String {
@@ -147,37 +138,37 @@ fn load_data(data_root: &String, ext: &str) -> String {
     "".to_string()
 }
 
-fn load_gz_file(data_root: &String, ext: &str) -> Result<String> {
-    let mut p = check_absolute_path(DATA_PATH);
-    p.push(data_root);
-    p = p.with_extension(ext);
-    let mut file = File::open(p)?;
-    let mut bytes = Vec::new();
-    file.read_to_end(&mut bytes)?;
-    let mut gz = GzDecoder::new(&bytes[..]);
-    let mut s = String::new();
-    gz.read_to_string(&mut s)?;
-    Ok(s)
+fn build_index_template(available_datasets: Vec<Dataset>) -> String {
+    let mut html = load_asset("index.html");
+    let outer_list_template = String::from("<li>{{dataset_name}}<ul>{{vcf_list}}</ul></li>");
+    let inner_list_template = String::from("<li><a href=\"/{{dataset_name}}/{{vcf_name}}\">{{vcf_name}}</a></li>");
+
+    let mut outer_list = String::from("");
+    for dataset in available_datasets {
+        let mut inner_list = String::from("");
+        for filename in dataset.vcf {
+            let inner_list_element = inner_list_template
+                .replace("{{dataset_name}}", &dataset.name)
+                .replace("{{vcf_name}}", &filename);
+            inner_list.push_str(&inner_list_element);
+        }
+        let outer_list_element = outer_list_template
+            .replace("{{dataset_name}}", &dataset.name)
+            .replace("{{vcf_list}}", &inner_list);
+        outer_list.push_str(&outer_list_element);
+    }
+    html = html.replace("{{dataset_list}}", &outer_list);
+    html
 }
 
 fn build_mutation_template(root: &String) -> String {
-//    1. read in all files:
-//    template
     let mut html = load_asset("mutations.html");
-//    js
     let d3 = load_asset("d3.min.js");
     let xlsx = load_asset("xlsx.core.min.js");
-//    vcf, blacklist, whitelist
-    let vcf = match load_gz_file(&root, "vcf.gz") {
-        Ok(content) => content,
-        Err(e) => {
-            eprintln!("{:?}", e);
-            "".to_string()
-        }
-    };
+    let vcf = load_data(&root, "vcf.gz");
     let blacklist = load_data(&root, "blacklist.tsv");
     let whitelist = load_data(&root, "whitelist.tsv");
-//    2. make all substitutions
+
     html = html
         .replace("{{xlsx_lib}}", &xlsx)
         .replace("{{d3_lib}}", &d3)
@@ -187,50 +178,42 @@ fn build_mutation_template(root: &String) -> String {
     html
 }
 
+// HANDLERS
+
+fn handle_index() -> Response {
+    let d = get_datasets(DATA_PATH);
+    match d {
+        Ok(available_datasets) => Response::html(build_index_template(available_datasets)),
+        Err(_) => Response::text("Could not list data directory.").with_status_code(500)
+    }
+}
+
+fn handle_viewer(dataset: String, vcf: String) -> Response {
+    let root = format!("{}/{}", dataset, vcf);
+    check_auxiliary_files(&root);
+    Response::html(build_mutation_template(&root))
+}
+
+#[derive(Deserialize, Debug)]
+struct Json {
+    a: String,
+    b: u32
+}
+
+fn handle_post(request: &Request) -> Response {
+        let json: Json = try_or_400!(json_input(request));
+    println!("{:?}", json);
+    Response::text("post to update_blacklist")
+}
+
 fn main() {
     let addr: String = format!("{}:{}", URL, PORT);
-
     println!("Server listening at {}", &addr);
-
     start_server(addr, move |request| {
         router!(request,
-                (GET)(/) => {
-                    let d = get_datasets(DATA_PATH);
-                    match d {
-                        Ok(available_datasets) => Response::html(build_index_template(available_datasets)),
-                        Err(_) => Response::text("Could not list data directory.").with_status_code(500)
-                    }
-//                    let mut cwd = current_dir().unwrap();
-//                    cwd.push(DATA_PATH);
-//                    println!("{:?}", cwd);
-//                    println!("{:?}", cwd.canonicalize());
-//                    Response::text("puppa")
-                },
-
-//            (GET)(/assets/{asset_type: String}/{asset_name: String}) => {
-//                let mut folder = assets_path;
-//                folder = match asset_type.as_ref() {
-//                    "js" | "css" => format!("{}/{}", &folder, asset_type),
-//                    _ => return Response::empty_404()
-//                };
-//                if let Some(request) = request.remove_prefix(format!("/assets/{}",&asset_type).as_ref()) {
-//                    return match_assets(&request, &folder);
-//                }
-//                Response::empty_404()
-//            },
-
-                (GET)(/{ dataset: String }/{ vcf: String }) => {
-                    let root = format!("{}/{}",&dataset,&vcf);
-                    // 1. check whitelist and blacklist files
-                    check_auxiliary_files(&root);
-                    // 2. generate template and return response
-                    Response::html(build_mutation_template(&root))
-                },
-
-                (POST)(/update_mutation_blacklist) => {
-                    Response::text("post to update_blacklist")
-                },
-
+                (GET)(/) => { handle_index() },
+                (GET)(/{ dataset: String }/{ vcf: String }) => { handle_viewer(dataset, vcf) },
+                (POST)(/update_mutation_blacklist) => { handle_post(request) },
                 _ => Response::empty_404()
         )
     });
